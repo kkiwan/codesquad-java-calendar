a4e06d8d25bee9cd9838a986d10e79ad4fe9fe61
김기완 <kiwan.kim@sungjinhds.com>
2021-04-29 13:34:12 +0900

    Implement printing calendar (auto version)

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,43 +1,91 @@
 package honux.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		else
+			return false;
 	}
 
-	public void printSampleCalendar() {
-		System.out.println("일  월  화  수 목  금  토");
-		System.out.println("--------------------");
-		System.out.println("1   2  3  4  5  6  7");
-		System.out.println("8   9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-
-	public static void main(String[] args) {
-		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
-
-		for (int i = 0; i < repeat; i++) {
-			System.out.println("달을 입력하세요");
-			int month = scanner.nextInt();
-
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month];
+		} else {
+			return MAX_DAYS[month];
 		}
-		
-		System.out.println("Bye~");
+	}
 
-//		cal.printSampleCalendar();
+	public void printCalendar(int year, int month) {
+		System.out.printf("    <<%d년 %d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
 
-		scanner.close();
+		// get weekday automatically
+		int weekday = getWeekDay(year, month, 1);
+
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+
+		// print first line
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		// print from second line to last
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim) {
+				System.out.println();
+			}
+		}
+
+		System.out.println();
+		System.out.println();
+	}
+
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 3; // 1970/Jan/1 = Thursday
+
+		int count = 0;
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+
+//		System.out.println(count);
+		for (int i = 1; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+
+		count += day;
+
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+
+		return weekday;
+	}
+
+	// simple test code here
+	public static void main(String[] args) {
+		Calendar cal = new Calendar();
+		System.out.println(cal.getWeekDay(1970, 1, 1) == 3);
+		System.out.println(cal.getWeekDay(1971, 1, 1) == 4);
+		System.out.println(cal.getWeekDay(1972, 1, 1) == 5);
+		System.out.println(cal.getWeekDay(1973, 1, 1) == 0);
+		System.out.println(cal.getWeekDay(1974, 1, 1) == 1);
 	}
 }
